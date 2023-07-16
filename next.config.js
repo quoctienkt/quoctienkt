96cae81723e0aa21e55e1c50f995144b2a417a13
TienDang <dangquoctienvktl@gmail.com>
2023-07-16 22:46:24 +0700

    feature/add gh-pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
     output: "export",
-    // basePath: "/quoctienkt",
-    // reactStrictMode: true,
 }
 
 module.exports = nextConfig
