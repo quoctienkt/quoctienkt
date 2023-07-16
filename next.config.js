4d3b2f661d143be6f4e43484a3cf2a4d1cf111aa
TienDang <dangquoctienvktl@gmail.com>
2023-07-16 22:32:02 +0700

    bugfix/update for gh-pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/quoctienkt"
+    // basePath: "/quoctienkt",
+    // reactStrictMode: true,
 }
 
 module.exports = nextConfig
