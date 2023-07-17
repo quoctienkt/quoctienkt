6e60edc1efd5308e03812489a1ee0ad61592754f
TienDang <dangquoctienvktl@gmail.com>
2023-07-17 21:11:24 +0700

    bugfix/gh-pages

--- next.config.js
+++ next.config.js
@@ -2,20 +2,22 @@
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-// let assetPrefix = ''
-// let basePath = '/'
+let assetPrefix = ""
+let basePath = ""
+let outputMode = undefined
 
-// if (isGithubActions) {
-//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-//   assetPrefix = `/${repo}/`
-//   basePath = `/${repo}`
-// }
+  assetPrefix = `/${repo}`
+  basePath = `/${repo}`
+  outputMode = "export"
+}
 
 const nextConfig = {
-    // assetPrefix: assetPrefix,
-    // basePath: basePath,
-    output: "export",
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    output: outputMode,
 }
 
 module.exports = nextConfig
---
+++ src/utils/AssetUtil.ts
@@ -0,0 +1,9 @@
+import { assetPrefix } from "../../next.config"
+
+const getAssetPath = (path: string): string => {
+    return `${assetPrefix}/${path}`
+}
+
+export {
+    getAssetPath
+}
