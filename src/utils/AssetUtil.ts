547455854d99b4b33d410e57812b7ab58e78aea1
TienDang <dangquoctienvktl@gmail.com>
2023-07-19 01:25:27 +0700

    bugfix/hand-writing-classification not working in production

--- src/utils/AssetUtil.ts
+++ src/utils/AssetUtil.ts
@@ -1,6 +1,7 @@
 import { assetPrefix } from "../../next.config"
 
 const getAssetPath = (path: string): string => {
+  console.log("assetPrefix: ", assetPrefix)
   return `${assetPrefix}/${path}`
 }
 
