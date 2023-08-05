27de240ab16ae67f844f82a035b756719b2376f6
TienDang <dangquoctienvktl@gmail.com>
2023-08-05 15:28:34 +0700

    bugfix/aboutUs responsive

--- src/components/layout/aboutUs/AboutUs.tsx
+++ src/components/layout/aboutUs/AboutUs.tsx
@@ -39,88 +39,84 @@ export function AboutUs() {
         toggle(!aboutUsExpansion, styles.wrapperCollapse)
       )}
     >
-      <div>TienDang Collection</div>
-      {verticalLine}
-      <Link
-        className="flex flex-row items-center gap-1"
-        href="https://www.facebook.com/dangquoctienvktl/"
-        target="_blank"
-      >
-        <AppImage
-          src="/img/icons8-facebook-48.png"
-          alt="Github Icon"
-          width={48}
-          height={48}
-        ></AppImage>
-        <span></span>
-      </Link>
-      <Link
-        className="flex flex-row items-center gap-1"
-        href="https://github.com/quoctienkt/quoctienkt/"
-        target="_blank"
-      >
-        <AppImage
-          src="/img/github-6980894_1280.png"
-          alt="Github Icon"
-          width={35}
-          height={35}
-        ></AppImage>
-        <span className={styles.hideOnCollapse}>Give me a star</span>
-      </Link>
-      {verticalLine}
-      {theme === "dark" && (
-        <div
-          className={styles.themeToggle}
-          onClick={() => {
-            setTheme("light");
-          }}
-        >
+      <div className={styles.me}>TienDang Collection</div>
+      <div className={styles.socialGroup}>
+        {verticalLine}
+        <Link href="https://www.facebook.com/dangquoctienvktl/" target="_blank">
           <AppImage
-            src="/img/brightness-and-contrast.png"
-            alt="Theme Config Light Icon"
-            width={35}
-            height={35}
+            src="/img/icons8-facebook-48.png"
+            alt="Facebook Icon"
+            width={48}
+            height={48}
           ></AppImage>
-        </div>
-      )}
-      {theme === "light" && (
-        <div
-          className={styles.themeToggle}
-          onClick={() => {
-            setTheme("dark");
-          }}
-        >
+          <span></span>
+        </Link>
+        <Link href="https://github.com/quoctienkt/quoctienkt/" target="_blank">
           <AppImage
-            src="/img/moon.png"
-            alt="Theme Config Moon Icon"
+            src="/img/github-6980894_1280.png"
+            alt="Github Icon"
             width={40}
             height={40}
           ></AppImage>
-        </div>
-      )}
+        </Link>
+      </div>
 
-      <div
-        className={styles.aboutUsExpansion}
-        onClick={() => setAboutUsExpansion(!aboutUsExpansion)}
-      >
-        {aboutUsExpansion && (
-          <AppImage
-            className="rotate-180 invert"
-            src="/img/to_top.png"
-            alt="About Us Collapse Icon"
-            width={40}
-            height={40}
-          ></AppImage>
+      <div className={styles.settingGroup}>
+        {verticalLine}
+        {theme === "dark" && (
+          <div
+            className={classes(styles.themeToggle, styles.flexItem)}
+            onClick={() => {
+              setTheme("light");
+            }}
+          >
+            <AppImage
+              src="/img/brightness-and-contrast.png"
+              alt="Theme Config Light Icon"
+              width={40}
+              height={40}
+            ></AppImage>
+          </div>
         )}
-        {!aboutUsExpansion && (
-          <AppImage
-            className="invert"
-            src="/img/to_top.png"
-            alt="About Us Expansion Icon"
-            width={40}
-            height={40}
-          ></AppImage>
+        {theme === "light" && (
+          <div
+            className={classes(styles.themeToggle, styles.flexItem)}
+            onClick={() => {
+              setTheme("dark");
+            }}
+          >
+            <AppImage
+              src="/img/moon.png"
+              alt="Theme Config Moon Icon"
+              width={40}
+              height={40}
+            ></AppImage>
+          </div>
         )}
+
+        <div
+          className={classes(styles.aboutUsExpansion, styles.flexItem)}
+          onClick={() => setAboutUsExpansion(!aboutUsExpansion)}
+        >
+          {aboutUsExpansion && (
+            <AppImage
+              className="rotate-180 invert"
+              src="/img/to_top.png"
+              alt="About Us Collapse Icon"
+              width={40}
+              height={40}
+            ></AppImage>
+          )}
+          {!aboutUsExpansion && (
+            <AppImage
+              className="invert"
+              src="/img/to_top.png"
+              alt="About Us Expansion Icon"
+              width={40}
+              height={40}
+            ></AppImage>
+          )}
+        </div>
       </div>
     </div>
   );
