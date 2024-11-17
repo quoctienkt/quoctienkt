310fccc4b9b00faa041bbfc8bb21214f826faa12
swotiendang <tien.dang@softwareone.com>
2024-11-17 20:51:09 +0700

    mono repo

--- app/src/pages/test2/page.tsx
+++ app/src/pages/test2/page.tsx
@@ -1,14 +1,21 @@
-// "use client"
-
 import { TEST } from "@quoctienkt/abc-module/src/utils/constants";
 // import App from "@quoctienkt/abc-module/src/App";
+import { useState } from "react";
+import React from "react";
 
 export default function Page() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <section className="flex justify-center w-full h-full bg-[#ffffff] overflow-auto relative sm:items-center">
-        <div className="sm:w-[721px] h-fit">{TEST}</div>
+        <div className="sm:w-[721px] h-fit">
+          {TEST}
+          <br />
+          {count}
+        </div>
       </section>
+
       {/* <App /> */}
     </>
   );
